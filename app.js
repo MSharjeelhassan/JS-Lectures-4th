6b5412b67a89c3d704df201d325691477e638c43
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2023-12-22 19:31:12 +0500

    lecture before

---
+++ app.js
@@ -0,0 +1,91 @@
+
+
+
+// var a = "Karachi";
+// a = a.toLowerCase();
+
+// var b =prompt("Enter city name");
+// b=b.toLowerCase();
+
+// if (a===b){
+//     console.log("City Matched");
+// // }
+
+// var cityNames=["Karachi", "lahr","islama","newyork","peshwar","lal haveli", "multan","Bahawalpur"];
+// var userWrd = prompt("Kuch tou bolo, kis city ka ho");
+
+// // userWrd.toLowerCase();
+// // cityNames.toLowerCase();
+
+
+// for(var i=0; i<cityNames.length; i++){
+//     if(userWrd===cityNames[i]){console.log("Acha acha")}else{console.log("hmain na pata apkey city ka")};
+
+//     }
+
+// sir ka code hy yeh jo nechay likha hy line 28 sa onward
+
+// var arr = ["karachi","isal","lahore","muscat"];
+// var ab=prompt("Enter City");
+// ab = ab.toLowerCase();
+
+// for (var i=0;i<Array.lemght;i++){
+//    arr[i]= arr[i].tolowerCase();
+// console.log(arr[i]);
+// }
+
+
+
+//     var a ="HARIS";
+//     var letter1=a.slice(0,1);
+//     letter1 =letter1.toUpperCase();
+
+//     var otherletters =a.slice(1);
+//     otherletters =otherletters.toLowerCase();
+
+// //     var word = letter1 = otherletters;
+// //     console.log(word);
+
+
+    // var a = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quae maiores voluptas Saturday porro dicta sint non ullam distinctio quisquam vero";
+
+    // var word = "Saturday";
+
+    // for(var i =0; i<a.length;i++){
+    //     var checkLetter = a.slice(i, i+8);
+    //     // i+8 main 8 is liye likha hy coz saturday main 8 letters hain
+    //     console.log(checkLetter);
+    //     if(checkLetter==word){
+    //         console.log("Word Found");
+    //     }
+    // }
+
+    // // is upar wali example main hum na var a main Sturday word found kiya hy.
+
+    // // Ab hm log is Saturday ko replace karenge new word sa
+
+    // var word = "Saturday";
+    // var secondWord = "Kal chhhuttti hai";
+    // var startingText;
+    // var endingText;
+
+    // for (var i=0; i<a.length;i++){
+    //     var checkLetter = a.slice(i, i+8);
+    //     if(checkLetter== word){
+    //         console.log(checkLetter,i);
+    //         startingText = a.slice(0,i);
+    //         endingText = a.slice(i+8);
+    //         console.log(startingText);
+    //         console.log(endingText);
+
+    var carNm ;
+
+
+    //     }
+    // }
+
+    // a = startingText + secondWord + endingText;
+
+    // console.log(a);
+
+//    now agr  main saturday v
